17f663a02a2aa608f5524f0d06527ed36a76e6be
Benedikt Schnupp <benediktschnupp@me.com>
2020-11-09 18:38:49 +0100

    cookie banner & gtag

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
+
+
 const Footer = class extends React.Component {
   render() {
     return (
       <footer style={{
         backgroundColor: "#000000"
       }}>
-        
       </footer>
     )
   }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,8 +6,30 @@ import './all.sass'
 import useSiteMetadata from './SiteMetadata'
 import { withPrefix } from 'gatsby'
 
+import CookieConsent from "react-cookie-consent";
+
+function setCookies() {
+    var s = document.createElement('script');
+    s.type = "text/javascript"
+    s.async = "true";
+    s.src = "https://www.googletagmanager.com/gtag/js?id=G-7G7CKXKSY1";
+    var x = document.getElementsByTagName('script')[0];
+    x.parentNode.insertBefore(s, x);
+};
+
 const TemplateWrapper = ({ children }) => {
   const { title, description } = useSiteMetadata()
+  var cookie = document.cookie
+  if (!!cookie) {
+    var gdpr = cookie
+      .split('; ')
+      .find(row => row.startsWith('gdpr'))
+      .split('=')[1];
+      console.log(gdpr);
+      if (!!gdpr === true) {
+        setCookies()
+    }
+  }
   return (
     <div>
       <Helmet>
@@ -49,10 +71,32 @@ const TemplateWrapper = ({ children }) => {
           property="og:image"
           content={`${withPrefix('/')}img/og-image.jpg`}
         />
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-7G7CKXKSY1"></script>
+
       </Helmet>
       <Navbar />
       <div>{children}</div>
       <Footer />
+      <CookieConsent
+          location="bottom"
+          buttonText="Accept"
+          buttonStyle={{backgroundColor: "rgba(255, 165, 2, 1)", borderRadius: 99, fontSize: "16px", color: "white", fontWeight: 900, padding: "8px 24px"}}
+          enableDeclineButton={true}
+          declineButtonText="Decline"
+          declineButtonStyle={{backgroundColor: "rgba(255, 165, 2, 0)", borderRadius: 99}}
+          cookieName="gdpr"
+          style={{ background: "rgba(30, 144, 255, 1)" }}
+          expires={150}
+          onAccept={() => {
+            setCookies()
+            // window.dataLayer = window.dataLayer || [];
+            // function gtag(){dataLayer.push(arguments);}
+            // gtag('js', new Date());
+            // gtag('config', '${GA_TRACKING_ID}', {'anonymize_ip': true});
+          }}
+        >
+          This website uses cookies to enhance the user experience.{" "}&#127850;
+        </CookieConsent>
     </div>
   )
 }
